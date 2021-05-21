9306a5707d16c227dda3137028227508306d7316
Shanta-Dutta <74420422+Shanta-Dutta@users.noreply.github.com>
2021-05-21 12:05:19 -0400

    first commit

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/repository/BlogPostRepository.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.TechTalentBlog.repository;
+
+import com.techtalentsouth.TechTalentBlog.model.BlogPost;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+
+    Iterable<BlogPost> findByOrderByDateDesc();
+
+
+}
---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/service/BlogPostService.java
@@ -0,0 +1,9 @@
+//package com.techtalentsouth.TechTalentBlog.service;
+//
+//import com.techtalentsouth.TechTalentBlog.model.BlogPost;
+//
+//public interface BlogPostService {
+//
+//
+//    BlogPost addNewBlogPost(Blogpost blogpost);
+//}
